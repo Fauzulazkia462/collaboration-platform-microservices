e33c72febde2e899a914cfaf013c57a50df5e2e4
Fauzulazkia462 <fauzulazkia2002@gmail.com>
2026-05-24 14:34:49 +0700

    [UPDATE] add more caching config

--- springboot-user-service/src/main/java/com/example/springboot_user_service/SpringbootUserServiceApplication.java
+++ springboot-user-service/src/main/java/com/example/springboot_user_service/SpringbootUserServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.springboot_user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringbootUserServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootUserServiceApplication.class, args);
 	}
-
 }
