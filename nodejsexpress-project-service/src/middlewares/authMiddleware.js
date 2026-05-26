254a4ff81ce72d476788c114e8fc082bcc556a84
Fauzulazkia462 <fauzulazkia2002@gmail.com>
2026-05-26 23:09:34 +0700

    [UPDATE] change the jwt verification specifially with HS512 algo

--- nodejsexpress-project-service/src/middlewares/authMiddleware.js
+++ nodejsexpress-project-service/src/middlewares/authMiddleware.js
@@ -11,7 +11,13 @@ function authMiddleware(req, res, next) {
     const token = authHeader.split(" ")[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET,
+            {
+                algorithms: ["HS512"]
+            }
+        );
 
         // attach user to request
         req.user = {
