9bb069a76b99fcfd902d1a0c12de7daa4f3664e8
Fauzulazkia462 <fauzulazkia2002@gmail.com>
2026-05-29 14:57:54 +0700

    [UPDTAE] validate token if it's blacklisted

--- django-activity-service/app/middleware/auth_middleware.py
+++ django-activity-service/app/middleware/auth_middleware.py
@@ -1,7 +1,14 @@
 import jwt
 import os
+import redis
 
 from fastapi import Header, HTTPException
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    decode_responses=True
+)
 
 def verify_token(authorization: str = Header(None)):
 
@@ -9,6 +16,10 @@ def verify_token(authorization: str = Header(None)):
         raise HTTPException(status_code=401, detail="Unauthorized")
 
     token = authorization.split(" ")[1]
+
+    # CHECK BLACKLIST
+    if redis_client.exists(f"blacklist:{token}"):
+        raise HTTPException(status_code=401, detail="Token is blacklisted")
 
     try:
         payload = jwt.decode(
