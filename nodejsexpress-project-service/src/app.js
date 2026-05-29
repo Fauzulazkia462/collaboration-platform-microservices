245f2891d2f6b908a9f1b93dd85564e8a25722f4
Fauzulazkia462 <fauzulazkia2002@gmail.com>
2026-05-29 14:57:31 +0700

    [UPDATE] allow some origins and validate token if it's blacklisted

--- nodejsexpress-project-service/src/app.js
+++ nodejsexpress-project-service/src/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 
 const projectRoutes = require('./routes/projectRoutes');
 
 const app = express();
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:3000"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- nodejsexpress-project-service/src/middlewares/authMiddleware.js
+++ nodejsexpress-project-service/src/middlewares/authMiddleware.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
+const { createClient } = require('redis');
 
-function authMiddleware(req, res, next) {
+const redis = createClient({
+    url: process.env.REDIS_URL || "redis://redis:6379"
+});
+
+redis.connect().catch(console.error);
+
+async function authMiddleware(req, res, next) {
 
     const authHeader = req.headers.authorization;
 
@@ -11,15 +18,18 @@ function authMiddleware(req, res, next) {
     const token = authHeader.split(" ")[1];
 
     try {
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET,
-            {
-                algorithms: ["HS512"]
-            }
-        );
+        // 1. CHECK BLACKLIST FIRST
+        const isBlacklisted = await redis.exists(`blacklist:${token}`);
 
-        // attach user to request
+        if (isBlacklisted) {
+            return res.status(401).json({ message: "Token is blacklisted" });
+        }
+
+        // 2. VERIFY JWT
+        const decoded = jwt.verify(token, process.env.JWT_SECRET, {
+            algorithms: ["HS512"]
+        });
+
         req.user = {
             id: decoded.sub,
             email: decoded.sub,
